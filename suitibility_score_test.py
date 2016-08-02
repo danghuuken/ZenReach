85bf3b3c79a3c643465c7a8ec8d8212f9bd16d36
danghuuken <danghuu.ken@gmail.com>
2016-08-02 01:01:18 -0700

    Adding first test cases for suitibility score.

---
+++ suitibility_score_test.py
@@ -0,0 +1,54 @@
+import unittest
+
+# Set of test to test the functionality of the sutibility score
+class SuitibilityScoreTest(unittest.TestCase):
+	def setUp(self):
+		# Set up the input file. Read the test file of names and then run our tests on them
+		pass
+
+	def tearDown(self):
+		# Close any connections to our test file.
+		pass
+
+	# Test to count the number of letters in a word
+	def test_count_number_of_letters(self):
+		self.fail('Finish the test')
+
+	# Determines if a word is Odd or Even 
+	def test_is_number_of_letters_odd_or_even(self):
+		self.fail('Finish the test')
+	
+	# Test of a count of number of vowels in a customers name
+	def test_count_number_of_vowels(self):
+		self.fail('Finish the test')
+
+	# Test of the number of consonants in the customer's name
+	def test_number_of_consonants(self):
+		self.fail('Finish the test')
+
+	# Test to check if the product # of letters is divisiable with the customers # of leters
+	def test_check_if_number_of_letters_are_divisiable(self):
+		self.fail('Finish the test')
+
+	# Test to see if the length of customers name is the same as the products name
+	def test_two_strings_have_the_same_length(self):
+		self.fail('Finish the test')
+
+	# Test to see if both product and customers have are even # of letters
+	def test_both_words_have_even_letters(self):
+		self.fail('Finish the test')
+
+	# Test to see if both product and customers have odd # of letters
+	def test_both_words_have_odd_number_of_letters(self):
+		self.fail('Finish the test')
+
+	# Test to see if the # of letters add up to a multiple of 10 
+	def test_number_of_letters_in_word_is_multiple_of_10(self):
+		self.fail('Finish the test')
+
+	# Test to see if the # of letters add up to a multiple of 5
+	def test_number_of_letters_in_word_is_multiple_of_5(self):
+		self.fail('Finish the test')
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
