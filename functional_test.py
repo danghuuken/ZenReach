e66bd34e87accc560feb763b058b50c2caa49500
danghuuken <danghuu.ken@gmail.com>
2016-08-02 22:43:59 -0700

    Adding script to run all test and also adding a non working functionaly test

---
+++ functional_test.py
@@ -0,0 +1,28 @@
+import unittest
+
+class HighestSuitibilityScoreTest(unittest.TestCase):
+	
+	# Read in test input file
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_taking_input_file_and_return_highest_SS_score_per_case(self):
+
+		#read in file
+
+		#loop through the amount of test cases there are 
+
+			#analyze the first set of test cases
+
+			#get a map of people's name and the product they associate with
+
+			# get the highest score of the case
+
+		self.fail('Finish functional test')
+
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
